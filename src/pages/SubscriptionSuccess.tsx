f862d87d2c32a72b6d78a1ce6ad69d6ead87368e
suneel-eng <simhadrisnl@gmail.com>
2023-10-08 11:29:07 +0530

    complete: subscription success page

--- src/pages/SubscriptionSuccess.tsx
+++ src/pages/SubscriptionSuccess.tsx
@@ -1,9 +1,9 @@
-import { Col, Container, Row } from "react-bootstrap";
-import { ArrowRight, Bank2, PeopleFill, SendFill } from "react-bootstrap-icons";
+import { Col, Container, Row, Card, Badge, Alert, Button, Form } from "react-bootstrap";
+import { ArrowRight, Bank2, Clipboard, Facebook, PeopleFill, PlusCircleFill, SendFill, Tv, Twitter, Whatsapp } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 
 export default function SubscriptionSuccess(): JSX.Element {
-    return <Container fluid>
+    return <Container fluid className="pb-5">
         <Row className="mt-5">
             <Col lg="9" className="m-auto">
                 <Row>
@@ -18,7 +18,7 @@ export default function SubscriptionSuccess(): JSX.Element {
                             <Row className="m-0 p-0 align-items-center">
                                 <Col lg="auto" className="m-0 p-0">Skip</Col>
                                 <Col lg="auto" className="m-0 p-0 ms-1">
-                                    <ArrowRight size={16}/>
+                                    <ArrowRight size={16} />
                                 </Col>
                             </Row>
                         </Link>
@@ -28,7 +28,7 @@ export default function SubscriptionSuccess(): JSX.Element {
                     <Col>
                         <Row className="m-0 p-0 p-3 align-items-center" style={{ border: "1px solid black" }}>
                             <Col className="m-0 p-0" lg="auto">
-                                <SendFill size={30}/>
+                                <SendFill size={30} />
                             </Col>
                             <Col className="m-0 p-0 ms-3">
                                 <span className="fw-bold">You can also invite friends on your subscription</span>
@@ -38,7 +38,7 @@ export default function SubscriptionSuccess(): JSX.Element {
                     <Col>
                         <Row className="m-0 p-0 p-3 align-items-center" style={{ border: "1px solid black" }}>
                             <Col className="m-0 p-0" lg="auto">
-                                <PeopleFill size={30}/>
+                                <PeopleFill size={30} />
                             </Col>
                             <Col className="m-0 p-0 ms-3">
                                 <span className="fw-bold">Co-subscribers pay to join your subscription</span>
@@ -48,10 +48,142 @@ export default function SubscriptionSuccess(): JSX.Element {
                     <Col>
                         <Row className="m-0 p-0 p-3 align-items-center" style={{ border: "1px solid black" }}>
                             <Col className="m-0 p-0" lg="auto">
-                                <Bank2 size={30}/>
+                                <Bank2 size={30} />
                             </Col>
                             <Col className="m-0 p-0 ms-3">
                                 <span className="fw-bold">I receive my transfer at the beginning of the month</span>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="m-0 p-0 mt-3 p-3 pb-5" style={{ border: "1px solid black" }}>
+                    <Col lg={11} className="m-auto m-0 p-0">
+                        <Row className="m-0 p-0 p-5 align-items-center">
+                            <Col lg="auto">
+                                <Card>
+                                    <Card.Body>
+                                        <Row className="m-0 p-0 justify-content-center">
+                                            <Col lg="auto">
+                                                <Tv size={30} />
+                                            </Col>
+                                        </Row>
+                                        <Card.Title className="mt-3">Disney + Hotstar Premium</Card.Title>
+                                        <hr />
+                                        <span className="fw-bold text-center d-block">&#8377; 20 /month</span>
+                                        <Row className="justify-content-center mt-3">
+                                            <Col lg="auto">
+                                                <Badge pill bg="primary">
+                                                    <Row className="m-0 p-0 align-items-center">
+                                                        <Col lg="auto" className="m-0 p-0">
+                                                            <PeopleFill />
+                                                        </Col>
+                                                        <Col lg="auto" className="m-0 p-0 ms-2">
+                                                            <span className="fw-bold">3 slots</span>
+                                                        </Col>
+                                                    </Row>
+                                                </Badge>
+                                            </Col>
+                                        </Row>
+                                        <span className="d-block text-center mt-3">Created 02/10/2023</span>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <span className="small">Send the link to your friends to start collecting money. Share widely to make sure you will have participants.</span>
+                                </Row>
+                                <Row className="mt-5 align-items-center">
+                                    <Col lg="auto">
+                                        <span className="fw-bold small">Copy Link</span>
+                                    </Col>
+                                    <Col className="m-0 p-0">
+                                        <Alert variant="info" className="p-1">
+                                            <Row className="m-0 p-0 align-items-center">
+                                                <Col>
+                                                    <span>https://www.subshare.com/join/a400b0d</span>
+                                                </Col>
+                                                <Col lg="auto">
+                                                    <Button>
+                                                        <Row className="m-0 p-0">
+                                                            <Col lg="auto" className="m-0 p-0">
+                                                                <Clipboard />
+                                                            </Col>
+                                                            <Col className="m-0 p-0 ms-2">
+                                                                <span>Copy</span>
+                                                            </Col>
+                                                        </Row>
+                                                    </Button>
+                                                </Col>
+                                            </Row>
+                                        </Alert>
+                                    </Col>
+                                </Row>
+                                <Row className="mt-3 bg-dark justify-content-end align-items-center p-2">
+                                    <Col>
+                                        <span className="fw-bold text-white">Share on social networks:</span>
+                                    </Col>
+                                    <Col lg="auto">
+                                        <Row className="align-items-center m-0 p-0">
+                                            <Col lg="auto" className="m-0 p-0 me-2">
+                                                <Button>
+                                                    <Facebook />
+                                                </Button>
+                                            </Col>
+                                            <Col lg="auto" className="m-0 p-0 me-2">
+                                                <Button>
+                                                    <Whatsapp />
+                                                </Button>
+                                            </Col>
+                                            <Col lg="auto" className="m-0 p-0 me-2">
+                                                <Button>
+                                                    <Twitter />
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row className="mt-3">
+                            <Col>
+                                <span className="d-block fw-bold">Send invitation to your friends</span>
+                                <span>Or fill their email and we will send them the invite</span>
+                            </Col>
+                        </Row>
+                        <Row className="mt-3">
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Control as="textarea" rows={5} value="Hello, I've just created a group to share my subscription to Disney + Hotstar Premium on Subshare, The subscription sharing platform. I'm happy to give you the oppertunity to join it! Follow this link and get instant access: https://www.subshare.com/join/a400b0d. Together we can enjoy it for just a small amount of price. Feel free to share it as well! ravi" />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row className="mt-3">
+                            <Col>
+                                <Form.Group>
+                                    <Form.Control type="text" placeholder="Ex. John Doe"></Form.Control>
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                            <Form.Group>
+                                    <Form.Control type="email" placeholder="john.doe@example.com"></Form.Control>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row className="mt-3">
+                            <Col lg="auto">
+                                <Button>
+                                    <Row className="m-0 p-0 align-items-center">
+                                        <Col className="m-0 p-0" lg="auto">
+                                            <PlusCircleFill/>
+                                        </Col>
+                                        <Col className="m-0 p-0 ms-2">Add another</Col>
+                                    </Row>
+                                </Button>
+                            </Col>
+                        </Row>
+                        <Row className="mt-5 justify-content-center">
+                            <Col lg="auto">
+                                <Button>Send invitation</Button>
                             </Col>
                         </Row>
                     </Col>
